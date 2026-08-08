d05ba12dc85f29ea60701f8560a75af42110049f
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2026-08-08 11:01:51 -0300

    Adicionando dns

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dns from 'dns';
 
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
